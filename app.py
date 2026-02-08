9b2a170bea17885324fafb59add9b63385f830b1
kunchapumohith-ctrl <kunchapumohith@gmail.com>
2026-02-08 13:33:38 +0530

    Fix langchain text splitter import for Streamlit Cloud

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import tempfile
 from pypdf import PdfReader
 
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_core.documents import Document
